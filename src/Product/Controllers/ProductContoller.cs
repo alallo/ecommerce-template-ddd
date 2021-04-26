d35151e7841bbf7ef0c591086c34bd3eeeed18e0
Alessandro Lallo <alessandro.lallo@gmail.com>
2021-04-26 14:14:46 +0100

    New solution and new product API project

---
+++ src/Product/Controllers/ProductContoller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Product.Controllers
+{
+    [ApiController]
+    [Route("product")]
+    [Produces("application/json")]
+    public class ProductContoller : ControllerBase
+    {
+
+        private readonly ILogger<ProductContoller> _logger;
+
+        public ProductContoller(ILogger<ProductContoller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetProductList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<int>> GetProductAsync()
+        {
+            return Ok(17);
+        }
+    }
+}
